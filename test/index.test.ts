d19cb342e7b738c6bf2803e854bf4a4a7f9d064f
David Krepsky <d.krepsky@outlook.com>
2021-04-09 00:22:06 -0300

    chore(wp): tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,8 +1,35 @@
 import { expect, test } from '@oclif/test';
+import * as fs from 'fs';
 
 import cmd = require('../src');
 
 describe('capacitor-set-version', () => {
+  beforeEach(() => {
+    const packageJson = JSON.stringify({ version: '1.2.3' }, null, 2);
+
+    const buildGradle = `
+    apply plugin: 'com.android.application'
+
+    android {
+        defaultConfig {
+            versionCode 8
+            versionName "0.0.7"
+        }
+    }`;
+  });
+
+  const infoPlist = `
+  <?xml version="1.0" encoding="UTF-8"?>
+  <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+  <plist version="1.0">
+    <dict>
+	      <key>CFBundleShortVersionString</key>
+	      <string>1.0</string>
+	      <key>CFBundleVersion</key>
+	      <string>1</string>
+	  </dict>
+  </plist>`;
+
   test
     .stdout()
     .do(() => cmd.run([]))
