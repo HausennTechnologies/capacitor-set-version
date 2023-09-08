42c9eac0b00e555ef7ff9e257a4c4133a18e1ac1
iamso <so@dev.so>
2023-09-08 10:50:50 +0200

    feat: fix check for capacitor config

--- src/common/utils-capacitor.ts
+++ src/common/utils-capacitor.ts
@@ -3,6 +3,7 @@ import * as path from 'path';
 import CustomError from './custom-error';
 
 export const CAPACITOR_CONFIG_TS_FILE = 'capacitor.config.ts';
+export const CAPACITOR_CONFIG_JS_FILE = 'capacitor.config.js';
 export const CAPACITOR_CONFIG_JSON_FILE = 'capacitor.config.json';
 
 export function checkForCapacitorProject(dir: string) {
@@ -22,11 +23,12 @@ function checkDirectoryExist(dir: string) {
 
 function checkCapacitorConfigExist(dir: string) {
   const configTsExist = fs.existsSync(path.join(dir, CAPACITOR_CONFIG_TS_FILE));
+  const configJsExist = fs.existsSync(path.join(dir, CAPACITOR_CONFIG_JS_FILE));
   const configJsonExist = fs.existsSync(path.join(dir, CAPACITOR_CONFIG_JSON_FILE));
 
-  if (!configJsonExist && !configTsExist) {
+  if (!configJsonExist && !configTsExist && !configJsExist) {
     throw new CustomError(
-      `Invalid CapacitorJS project: neither ${CAPACITOR_CONFIG_TS_FILE} nor ${CAPACITOR_CONFIG_TS_FILE} exist in folder ${dir}`,
+      `Invalid CapacitorJS project: neither ${CAPACITOR_CONFIG_TS_FILE}, nor ${CAPACITOR_CONFIG_JS_FILE} or ${CAPACITOR_CONFIG_JSON_FILE} exist in folder ${dir}`,
       { code: 'ERR_PROJECT', suggestions: ['Make sure you are passing the right project path'] },
     );
   }
